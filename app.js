284f43c63ba083de06ff9cdcd259fff8578542d8
Hmiku39 <hachuneyuki@icloud.com>
2024-11-24 01:19:38 +0900

    投稿内容の降順表示、ポストテーブルとアカウントテーブルDBの結合、GOODボタン機能の実装

--- app.js
+++ app.js
@@ -27,14 +27,12 @@ app.use(
 );
 //ログインチェック
 app.use((req, res, next) => {
-    if (req.session.userId === undefined) {
-        console.log('ゲストアクセス');
-        res.locals.name = 'ゲスト';
+    if (req.session.acountNum === undefined) {
         res.locals.isLoggedIn = false;
+        console.log('ゲストアクセス');
     } else {
         console.log('ログイン成功');
-        res.locals.name = req.session.name;
-        console.log('ユーザ：' + req.session.name);
+        console.log('ユーザ：' + req.session.acountNum);
         res.locals.isLoggedIn = true;
     }
     next();
@@ -45,10 +43,10 @@ app.get('/', (req, res) => {
     const today = new Date();
     // console.log(todayTime);
     connection.query(
-        'SELECT * FROM post ',
+        'SELECT * FROM post INNER JOIN acount ON post.acountNum = acount.acountNum AND deleteFlg = "0" ORDER BY datetime DESC',
         (error, results) => {
-            // console.log(results);
-            // console.log(error);
+            console.log(results);
+            console.log(error);
             res.render('index.ejs',{posts: results, today: today});
         }
     );
@@ -56,10 +54,10 @@ app.get('/', (req, res) => {
 
 //投稿ページ
 app.get('/post', (req, res) => {
-    if (res.locals.isLoggedIn === true){
-        res.render('post.ejs');
-    } else {
+    if (req.session.acountNum === undefined){
         res.render('login.ejs', {formErrors: [], loginError: false});
+    } else {
+        res.render('post.ejs');
     }
 });
 
@@ -69,12 +67,11 @@ app.post('/createPost',(req, res) => {
     const postTime = date.toFormat('YYYYMMDDHH24MISS');//投稿日時取得
     console.log(postTime);
     connection.query(
-        'INSERT INTO post (name, content, datetime) VALUES (?, ?, ?)',
-        [req.session.name, req.body.content, postTime],
+        'INSERT INTO post (acountNum, content, datetime) VALUES (?, ?, ?)',
+        [req.session.acountNum, req.body.content, postTime],
         (error, results) => {
-            // console.log(results);
             console.log(error);
-            res.redirect('/')
+            res.redirect('/');
         })
     }
 );
@@ -86,22 +83,26 @@ app.get('/signup', (req, res) => {
 
 //アカウント登録処理
 app.post('/signup', (req, res, next) => {
-    const name = req.body.name;
+    const userid = req.body.userid;
+    const displayName = req.body.displayName;
     const email = req.body.email;
     const password = req.body.password;
     const agree = req.body.agree;
     const errors = [];
-    if(name === ''){
+    if(userid === ''){
         errors[0] = true;
       }
-      if(email === ''){
+      if(displayName === ''){
         errors[1] = true;
       }
-      if(password === ''){
+      if(email === ''){
         errors[2] = true;
       }
-      if(agree === undefined){
+      if(password === ''){
         errors[3] = true;
+      }
+      if(agree === undefined){
+        errors[4] = true;
       }
       if(errors.length > 0){
         res.render('signup.ejs', {errors: errors});
@@ -110,22 +111,23 @@ app.post('/signup', (req, res, next) => {
       }
 },
     (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    const date = new Date();
-    const signupDate = date.toFormat('YYYYMMDDHH24MISS');//投稿日時取得
+        const userid = req.body.userid;
+        const displayName = req.body.displayName;
+        const email = req.body.email;
+        const password = req.body.password;
+        const date = new Date();
+        const signupDate = date.toFormat('YYYYMMDDHH24MISS');//投稿日時取得
 
-    connection.query(
-        'INSERT INTO acount (name, email, password, signupDate) VALUES (?, ?, ? ,?)',
-        [name, email, password, signupDate],
-        (error, results) => {
-            req.session.userId = results.insertId;
-            req.session.name = name;
-            res.redirect('/');
-        }
-    );
-});
+        connection.query(
+            'INSERT INTO acount (userId, displayName, email, password, signupDate) VALUES (?, ?, ? ,?, ?)',
+            [userid, displayName, email, password, signupDate],
+            (error, results) => {
+                req.session.acountNum = results.insertId;
+                res.redirect('/');
+            }
+        );
+    }
+);
 
 //ログインページ
 app.get('/login', (req, res) => {
@@ -133,7 +135,7 @@ app.get('/login', (req, res) => {
 });
 
 //ログイン処理
-app.post('/login', (req, res, next) => {
+app.post('/login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     const formErrors = [];
@@ -156,8 +158,7 @@ app.post('/login', (req, res, next) => {
                 console.log(results.length);
                 if (results.length > 0) {
                     if (req.body.password === results[0].password){
-                        req.session.userId = results[0].id;
-                        req.session.name = results[0].name;
+                        req.session.acountNum = results[0].acountNum;
                         res.redirect('/');
                     } else {
                         res.render('login.ejs', {formErrors: formErrors, loginError: true});
@@ -168,6 +169,20 @@ app.post('/login', (req, res, next) => {
             }
         );
     }
+});
+
+//GOOD機能
+app.get('/good/:postNum', (req, res) => {
+    const postNum = req.params.postNum;
+    connection.query(
+        'UPDATE post SET good = good + 1 WHERE post.postNum = ?;',
+        [postNum],
+        (error, results) => {
+            console.log(results);
+            console.log(error);
+            res.redirect('/');
+        }
+    );
 });
 
 app.get('/test', (req, res) => {
